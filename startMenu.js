e833588609f6ca3a7666a4bab2bc78d1b1264763
Vince <vmsusini@gmail.com>
2019-07-31 17:23:33 -0400

    Game Screen Ready
    Fixed player choosing,  added new function to change screen views from menu to game
    
    DELETED: script.js

--- startMenu.js
+++ startMenu.js
@@ -1,3 +1,5 @@
+var NUMCHAR = 27
+
 var playerCount = window.prompt("How many players would you like?");
 alert("You chose " + playerCount + " Players");
 
@@ -14,116 +16,143 @@ function choosePlayer(num) {
     switch (num) {
         case 0:
             console.log("You picked Kim Jong Un");
-            createPlayer(0, characterNames[0],characterImages[0]);
             document.getElementById("char1").className += " selected";
+            createPlayer(0, characterNames[0],characterImages[0]);
             break;
         case 1:
             console.log("You picked Storm Trooper");
-            createPlayer(1);
+            document.getElementById("char2").className += " selected";
+            createPlayer(1, characterNames[1],characterImages[1]);
             break;
         case 2:
             console.log("You picked Dildo");
-            createPlayer(2);
+            document.getElementById("char3").className += " selected";
+            createPlayer(2, characterNames[2],characterImages[2]);
             break;
         case 3:
             console.log("You Picked Master Cheif");
-            createPlayer(3);
+            document.getElementById("char4").className += " selected";
+            createPlayer(3, characterNames[3],characterImages[3]);
             break;
         case 4:
             console.log("You Picked Bran");
-            createPlayer(4);
+            document.getElementById("char5").className += " selected";
+            createPlayer(4, characterNames[4],characterImages[4]);
             break;
         case 5:
             console.log("You Picked Crimson Chin");
-            createPlayer(5);
+            document.getElementById("char6").className += " selected";
+            createPlayer(5, characterNames[5],characterImages[5]);
             break;
         case 6:
             console.log("You Picked Thor");
-            createPlayer(6);
+            document.getElementById("char7").className += " selected";
+            createPlayer(6, characterNames[6],characterImages[6]);
             break;
         case 7:
             console.log("You Picked Houmoninia");
-            createPlayer(7);
+            document.getElementById("char8").className += " selected";
+            createPlayer(7, characterNames[7],characterImages[7]);
             break;
         case 8:
             console.log("You Picked Ghost");
-            createPlayer(8);
+            document.getElementById("char9").className += " selected";
+            createPlayer(8, characterNames[8],characterImages[8]);
             break;
         case 9:
             console.log("You Picked Camel Toe");
-            createPlayer(9);
+            document.getElementById("char10").className += " selected";
+            createPlayer(9, characterNames[9],characterImages[9]);
             break;
         case 10:
             console.log("You Picked Bad Luck Brian");
-            createPlayer(10);
+            document.getElementById("char11").className += " selected";
+            createPlayer(10, characterNames[10],characterImages[10]);
             break;
         case 11:
             console.log("You Picked Travis Scott");
-            createPlayer(11);
-            break;
+            document.getElementById("char12").className += " selected";
+            createPlayer(11, characterNames[11],characterImages[11]);
         case 12:
             console.log("Picked player 12");
-            createPlayer(12);
+            document.getElementById("char13").className += " selected";
+            createPlayer(12, characterNames[12],characterImages[12]);
             break;
         case 13:
             console.log("Picked player 13");
-            createPlayer(13);
+            document.getElementById("char14").className += " selected";
+            createPlayer(13, characterNames[13],characterImages[13]);
             break;
         case 14:
             console.log("Picked player 14");
-            createPlayer(14);
+            document.getElementById("char15").className += " selected";
+            createPlayer(14, characterNames[14],characterImages[14]);
             break;
         case 15:
             console.log("Picked player 15");
-            createPlayer(15);
+            document.getElementById("char16").className += " selected";
+            createPlayer(15, characterNames[15],characterImages[15]);
             break;
         case 16:
             console.log("Picked player 16");
-            createPlayer(16);
+            document.getElementById("char17").className += " selected";
+            createPlayer(16, characterNames[16],characterImages[16]);
             break;
         case 17:
             console.log("Picked player 17");
-            createPlayer(17);
+            document.getElementById("char18").className += " selected";
+            createPlayer(17, characterNames[17],characterImages[17]);
             break;
         case 18:
             console.log("Picked player 18");
-            createPlayer(18);
+            document.getElementById("char19").className += " selected";
+            createPlayer(18, characterNames[18],characterImages[18]);
             break;
         case 19:
             console.log("Picked player 19");
-            createPlayer(19);
+            document.getElementById("char20").className += " selected";
+            createPlayer(19, characterNames[19],characterImages[19]);
             break;
         case 20:
             console.log("Picked player 20");
-            createPlayer(20);
+            document.getElementById("char21").className += " selected";
+            createPlayer(20, characterNames[20],characterImages[20]);
             break;
         case 21:
             console.log("Picked player 21");
-            createPlayer(21);
+            document.getElementById("char22").className += " selected";
+            createPlayer(21, characterNames[21],characterImages[21]);
             break;
         case 22:
             console.log("Picked player 22");
-            createPlayer(22);
+            document.getElementById("char23").className += " selected";
+            createPlayer(22, characterNames[22],characterImages[22]);
             break;
         case 23:
             console.log("Picked player 23");
-            createPlayer(23);
+            document.getElementById("char24").className += " selected";
+            createPlayer(23, characterNames[23],characterImages[23]);
             break;
         case 24:
             console.log("Picked player 24");
-            createPlayer(24);
+            document.getElementById("char25").className += " selected";
+            createPlayer(24, characterNames[24],characterImages[24]);
             break;
         case 25:
-            console.log("Picked player 25");
-            createPlayer(25);
-            break;
-        case 26:
-            console.log("Picked player 26");
-            createPlayer(26);
+            console.log("Picked 25");
+            document.getElementById("char26").className += " selected";
+            createPlayer(25, characterNames[25],characterImages[25]);
             break;
         default:
             console.log("There was an error choosing a character");
+            alert("Problem, please restart.");
             return -1;
+    }
+}
+
+function makeAllPlayerSelected(){
+    for (var i = 1; i < NUMCHAR; i++){
+        document.getElementById("char"+i).className += " selected";
     }
 }
 
@@ -157,6 +186,14 @@ class Player {
     }
 }
 
+function changeScreen(){
+    var charSelection = document.getElementById("charSelection");
+    var gameScreen = document.getElementById("gameScreen");
+
+    charSelection.style.display = "none";
+    gameScreen.style.display = "block";
+}
+
 function createPlayer(num, name, imageName) {
     let player = new Player(currentPlayerCount, name, 0,imageName);
     playerList[currentPlayerCount] = player;
@@ -164,8 +201,9 @@ function createPlayer(num, name, imageName) {
     currentPlayerCount++;
     console.log("currentPLayerOCunt " + currentPlayerCount + " Player count " + playerCount);
     if (currentPlayerCount == playerCount) {
-        alert("Starting the Game");
         console.log(playerList);
+        makeAllPlayerSelected();
+        changeScreen(); 
     } else {
         console.log("Missed it");
     }
